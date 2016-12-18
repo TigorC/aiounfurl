d21967a0cf0de9c3c9b52d8b36d719865f5cfaf8
Igor Tokarev <TigorC@gmail.com>
2016-12-18 16:34:20 +0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setup(
     author="Igor Tokarev",
     author_email="TigorC@gmail.com",
     description="Extract information from web page",
-    license="BSD",
-    keywords="example documentation tutorial",
-    url="http://packages.python.org/an_example_pypi_project",
+    license="BSD License",
+    keywords="async embed preview",
+    url="https://github.com/tigorc/aiounfurl",
     include_package_data=True,
     install_requires=[
         'setuptools',
@@ -18,8 +18,12 @@ setup(
         'aiohttp'],
     packages=['aiounfurl'],
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Internet :: WWW/HTTP"
     ],
 )
